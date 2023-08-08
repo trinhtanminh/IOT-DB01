762493cad7237d4cc892e6795ebb66feabf99a72
Trịnh Tấn Minh <minh.207pm51630@vanlanguni.vn>
2023-08-08 14:20:55 +0700

    first commit

--- hello.py
+++ hello.py
@@ -1,2 +1,31 @@
-print ("Hello")
-print ("1233")
+import sys
+from Adafruit_IO import MQTTClient
+
+AIO_FEED_ID = "cambien1"
+AIO_USERNAME = "trinhtanmminh"
+AIO_KEY = "aio_KRzU12AwXZTmQr9vYPQzQLxyJpXR"
+
+def connected(client):
+    print("Ket noi thanh cong ...")
+    client.subscribe(AIO_FEED_ID)
+
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload)
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
+while True:
+    pass
